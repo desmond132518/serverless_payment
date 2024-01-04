83dbf83c03a59c0d85672671b95ac13fd9bec152
austin0715 <43836660+austin0715@users.noreply.github.com>
2024-01-04 15:44:00 -0500

    test

--- netlify/functions/stripe.js
+++ netlify/functions/stripe.js
@@ -2,29 +2,29 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 exports.handler = async (event, context) => {
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: "T-shirt",
-          },
-          unit_amount: 2000,
-        },
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    success_url: "https://serverless-payments.netlify.app/success",
-    cancel_url: "https://serverless-payments.netlify.app/cancel",
-  });
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: [
+            {
+                price_data: {
+                    currency: "cad",
+                    product_data: {
+                        name: "Cake",
+                    },
+                    unit_amount: 20,
+                },
+                quantity: 1,
+            },
+        ],
+        mode: "payment",
+        success_url: "https://serverless-payments.netlify.app/success",
+        cancel_url: "https://serverless-payments.netlify.app/cancel",
+    });
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      id: session.id,
-    }),
-  };
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            id: session.id,
+        }),
+    };
 };
