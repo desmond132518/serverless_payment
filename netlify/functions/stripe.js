66204778be983d2b7c46a071be96c7973a576ff0
austin0715 <43836660+austin0715@users.noreply.github.com>
2024-01-04 16:04:47 -0500

    change URLs

--- netlify/functions/stripe.js
+++ netlify/functions/stripe.js
@@ -1,5 +1,6 @@
 // Manage your variables with style: https://www.netlify.com/blog/2021/07/12/managing-environment-variables-from-your-terminal-with-netlify-cli/
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
+console.log(process.env.STRIPE_SECRET);
 
 exports.handler = async (event, context) => {
     const session = await stripe.checkout.sessions.create({
@@ -11,14 +12,14 @@ exports.handler = async (event, context) => {
                     product_data: {
                         name: "Cake",
                     },
-                    unit_amount: 20,
+                    unit_amount: 2000,
                 },
                 quantity: 1,
             },
         ],
         mode: "payment",
-        success_url: "https://serverless-payments.netlify.app/success",
-        cancel_url: "https://serverless-payments.netlify.app/cancel",
+        success_url: "https://serverless-payment.netlify.app/success",
+        cancel_url: "https://serverless-payment.netlify.app/cancel",
     });
 
     return {
